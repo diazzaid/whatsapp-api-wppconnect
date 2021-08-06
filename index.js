40341da984d2548f3a85af5851f70be9bc297dce
diazzaid <diaz.zaid20@gmail.com>
2021-08-06 15:15:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ async function startWPP (){
         console.log('Session name: ', session);
     },
         headless: false, // Headless chrome
-        devtools: false, // Open devtools by default
+        devtools: true, // Open devtools by default
         useChrome: false, // If false will use Chromium instance
         debug: false, // Opens a debug session
         logQR: true, // Logs QR automatically in terminal
@@ -135,7 +135,7 @@ async function startWPP (){
                     '--disable-accelerated-mjpeg-decode',
                     '--disable-app-list-dismiss-on-blur',
                     '--disable-accelerated-video-decode',
-				    '--single-process', // <- this one doesn't works in Windows
+		    '--single-process' // <- this one doesn't works in Windows
 		], 
 		
 		// Parameters to be added into the chrome browser instance
@@ -155,9 +155,71 @@ async function startWPP (){
 async function start(client) {
     instance = client; //It will be used in REST requests
 
-    client.onMessage( async (message) => {
+    //client.onMessage( async (message) => {
+	  client.onMessage(async (msg) => {
+    try {
+      if (msg.body == '!ping') {
+        // Send a new message to the same chat
+        client.sendText(msg.from, 'pong');
+	  } else if (msg.body == 'hai') {
+        // Send a new message to the same chat
+        client.sendText(msg.from, 'hallo');
+      } else if (msg.body == '!ping reply') {
+        // Send a new message as a reply to the current one
+        client.reply(msg.from, 'pong', msg.id.toString());
+      } else if (msg.body == '!chats') {
+        const chats = await client.getAllChats();
+        client.sendText(msg.from, `The bot has ${chats.length} chats open.`);
+      } else if (msg.body == '!info') {
+        let info = await client.getHostDevice();
+        let message = `_*Connection info*_\n\n`;
+        message += `*User name:* ${info.pushname}\n`;
+        message += `*Number:* ${info.wid.user}\n`;
+        message += `*Battery:* ${info.battery}\n`;
+        message += `*Plugged:* ${info.plugged}\n`;
+        message += `*Device Manufacturer:* ${info.phone.device_manufacturer}\n`;
+        message += `*WhatsApp version:* ${info.phone.wa_version}\n`;
+        client.sendText(msg.from, message);
+      } else if (msg.body.startsWith('!sendto ')) {
+        // Direct send a new message to specific id
+        let number = msg.body.split(' ')[1];
+        let messageIndex = msg.body.indexOf(number) + number.length;
+        let message = msg.body.slice(messageIndex, msg.body.length);
+        number = number.includes('@c.us') ? number : `${number}@c.us`;
+        client.sendText(number, message);
+      } else if (msg.body.startsWith('!pin ')) {
+        let option = msg.body.split(' ')[1];
+        if (option == 'true') {
+          await client.pinChat(msg.from, true);
+        } else {
+          await client.pinChat(msg.from, false);
+        }
+      } else if (msg.body.startsWith('!typing ')) {
+        const option = msg.body.split(' ')[1];
+        if (option == 'true') {
+          // Start typing...
+          await client.startTyping(msg.from);
+        } else {
+          // Stop typing
+          await client.stopTyping(msg.from);
+        }
+      } else if (msg.body.startsWith('!ChatState ')) {
+        const option = msg.body.split(' ')[1];
+        if (option == '1') {
+          await client.setChatState(msg.from, '0');
+        } else if (option == '2') {
+          await client.setChatState(msg.from, '1');
+        } else {
+          await client.setChatState(msg.from, '2');
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  
 
-    }); 
+    //}); 
     client.onAck(ack => {
 
     });
